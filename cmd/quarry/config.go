63b8bd01ff69a3bd5143744a3bb53f087e771d7f
Szymon <sz.oleskiewicz@gmail.com>
2025-05-30 14:45:18 +0200

    Changed data types

--- cmd/quarry/config.go
+++ cmd/quarry/config.go
@@ -8,30 +8,30 @@ import (
 )
 
 type SimulationConfig struct {
-	WorkersAmount         [3]int `json:"workers_amount"`
-	StonesExtractionTime  [3]int `json:"stones_extraction_time"`
-	StonesMasses          [3]int `json:"stones_masses"`
-	StoneMassesLimits     [3]int `json:"stone_masses_limits"`
-	TimeToTravelEmpty     int    `json:"time_to_travel_empty"`
-	TimeToTravelFull      int    `json:"time_to_travel_full"`
-	QuarryWorkplaces      int    `json:"quarry_workplaces"`
-	TimeToPlaceStone      int    `json:"time_to_place_stone"`
-	TimeToPlaceInsulation int    `json:"time_to_place_insulation"`
-	TimeToChangePallet    int    `json:"time_to_change_pallet"`
+	WorkersAmount         [3]int    `json:"workers_amount"`
+	StonesExtractionTime  [3][2]int `json:"stones_extraction_time"`
+	StonesMasses          [3]int    `json:"stones_masses"`
+	StoneMassesLimits     [3]int    `json:"stone_masses_limits"`
+	TimeToTravelEmpty     [2]int    `json:"time_to_travel_empty"`
+	TimeToTravelFull      [2]int    `json:"time_to_travel_full"`
+	QuarryWorkplaces      int       `json:"quarry_workplaces"`
+	TimeToPlaceStone      [2]int    `json:"time_to_place_stone"`
+	TimeToPlaceInsulation [2]int    `json:"time_to_place_insulation"`
+	TimeToChangePallet    [2]int    `json:"time_to_change_pallet"`
 }
 
 func defaultConfig() SimulationConfig {
 	return SimulationConfig{
-		WorkersAmount:         [3]int{3, 3, 3},
-		StonesExtractionTime:  [3]int{4, 4, 4},
+		WorkersAmount:         [3]int{1, 1, 1},
+		StonesExtractionTime:  [3][2]int{{1000, 2500}, {1000, 2500}, {3000, 4500}},
 		StonesMasses:          [3]int{1, 3, 5},
 		StoneMassesLimits:     [3]int{14, 13, 11},
-		TimeToTravelEmpty:     500,
-		TimeToTravelFull:      1200,
+		TimeToTravelEmpty:     [2]int{50, 250},
+		TimeToTravelFull:      [2]int{200, 500},
 		QuarryWorkplaces:      4,
-		TimeToPlaceStone:      300,
-		TimeToPlaceInsulation: 500,
-		TimeToChangePallet:    1000}
+		TimeToPlaceStone:      [2]int{300, 1500},
+		TimeToPlaceInsulation: [2]int{500, 1000},
+		TimeToChangePallet:    [2]int{1000, 2000}}
 }
 
 func getConfig() SimulationConfig {
@@ -39,7 +39,7 @@ func getConfig() SimulationConfig {
 	conf := SimulationConfig{}
 	err = json.Unmarshal(read, &conf)
 	if err != nil {
-		fmt.Println("No config found, creating one with default values")
+		//fmt.Println("No config found, creating one with default values")
 		conf := defaultConfig()
 		file, err := os.Create("./config/config.json")
 		str, err := json.Marshal(conf)
@@ -56,12 +56,13 @@ func getConfig() SimulationConfig {
 
 }
 
-func (conf SimulationConfig) printConfig() {
-	fmt.Println("Current config:")
+func (conf SimulationConfig) printConfig() string {
+	out := "Current config:\nq"
 	v := reflect.ValueOf(conf)
 	typeOfConf := v.Type()
 	for i := 0; i < typeOfConf.NumField(); i++ {
-		fmt.Printf("%s: %v\n", typeOfConf.Field(i).Name, v.Field(i).Interface())
+		out += fmt.Sprintf("%s: %v\n", typeOfConf.Field(i).Name, v.Field(i).Interface())
 	}
-	fmt.Println("")
+	out += "\n"
+	return out
 }
